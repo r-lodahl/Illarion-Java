db62a4de7929ba7ae233a1f5d296aa46d2683850
r-lodahl <sejyiahn@gmail.com>
2020-12-10 20:38:36 +0100

    Removed config responsibility from the IllaClient

--- illaclient/src/main/java/illarion/client/IllaClient.java
+++ illaclient/src/main/java/illarion/client/IllaClient.java
@@ -19,12 +19,14 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.util.ContextInitializer;
 import ch.qos.logback.core.joran.spi.JoranException;
 import ch.qos.logback.core.util.StatusPrinter;
+import com.google.common.eventbus.Subscribe;
 import illarion.client.crash.DefaultCrashHandler;
 import illarion.client.net.client.LogoutCmd;
 import illarion.client.resources.SongFactory;
 import illarion.client.resources.SoundFactory;
 import illarion.client.resources.loaders.SongLoader;
 import illarion.client.resources.loaders.SoundLoader;
+import illarion.client.state.StateManager;
 import illarion.client.util.ChatLog;
 import illarion.client.util.GlobalExecutorService;
 import illarion.client.util.Lang;
@@ -42,12 +44,11 @@ import illarion.common.util.Crypto;
 import illarion.common.util.DirectoryManager;
 import illarion.common.util.DirectoryManager.Directory;
 import illarion.common.util.TableLoader;
-import org.bushe.swing.event.*;
-import org.illarion.engine.Backend;
-import org.illarion.engine.DesktopGameContainer;
-import org.illarion.engine.EngineException;
-import org.illarion.engine.EngineManager;
-import org.illarion.engine.graphic.GraphicResolution;
+import org.illarion.engine.EventBus;
+import org.illarion.engine.GameContainer;
+import org.illarion.engine.backend.gdx.ApplicationGameContainer;
+import org.illarion.engine.event.GameExitEnforcedEvent;
+import org.illarion.engine.event.WindowResizedEvent;
 import org.jetbrains.annotations.Contract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,28 +60,25 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Locale;
+import java.util.*;
 import java.util.Locale.Category;
-import java.util.Objects;
 import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * Main Class of the Illarion Client, this loads up the whole game and runs the main loop of the Illarion Client.
- *
- * @author Nop
- * @author Martin Karing &lt;nitram@illarion.org&gt;
  */
-public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent> {
+public final class IllaClient {
     /**
      * The identification of this application.
      */
     @Nonnull
-    public static final AppIdent APPLICATION = new AppIdent("Illarion Client"); //$NON-NLS-1$
+    public static final AppIdent APPLICATION = new AppIdent("Illarion"); //$NON-NLS-1$
     @Nonnull
     public static final String CFG_FULLSCREEN = "fullscreen";
     @Nonnull
-    public static final String CFG_RESOLUTION = "resolution";
+    public static final String CFG_WIDTH = "windowWidth";
+    @Nonnull
+    public static final String CFG_HEIGHT = "windowHeight";
     /**
      * The default server the client connects too. The client will always connect to this server.
      */
@@ -97,38 +95,23 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
     @Nonnull
     private static final Logger LOGGER = LoggerFactory.getLogger(IllaClient.class);
 
-    /**
-     * Stores if there currently is a exit requested to avoid that the question area is opened multiple times.
-     */
-    private static boolean exitRequested;
-
     static {
         String server = System.getProperty("illarion.server", "realserver");
         switch ((server == null) ? "" : server) {
-            case "testserver":
-                DEFAULT_SERVER = Servers.Testserver;
-                break;
-            case "devserver":
-                DEFAULT_SERVER = Servers.Devserver;
-                break;
-            default:
-                DEFAULT_SERVER = Servers.Illarionserver;
-                break;
+            case "testserver" -> DEFAULT_SERVER = Servers.Testserver;
+            case "devserver" -> DEFAULT_SERVER = Servers.Devserver;
+            default -> DEFAULT_SERVER = Servers.Illarionserver;
         }
     }
 
     /**
      * The configuration of the client settings.
      */
-    private ConfigSystem cfg;
-    /**
-     * This is the reference to the Illarion Game instance.
-     */
-    private Game game;
+    private ConfigSystem config;
     /**
      * The container that is used to display the game.
      */
-    private DesktopGameContainer gameContainer;
+    private GameContainer gameContainer;
     /**
      * Stores the server the client shall connect to.
      */
@@ -142,23 +125,11 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
     }
 
     /**
-     * Show a question frame if the client shall really quit and exit the client in case the user selects yes.
-     */
-    public static void ensureExit() {
-        if (exitRequested) {
-            return;
-        }
-        exitRequested = true;
-
-        INSTANCE.game.enterState(Game.STATE_ENDING);
-    }
-
-    /**
      * Show an error message and leave the client.
      *
      * @param message the error message that shall be displayed.
      */
-    public static void errorExit(@Nonnull String message) {
+    public static void exitWithError(@Nonnull String message) {
         World.cleanEnvironment();
 
         LOGGER.info("Client terminated on user request.");
@@ -166,7 +137,7 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
         LOGGER.error(message);
         LOGGER.error("Terminating client!");
 
-        INSTANCE.cfg.save();
+        INSTANCE.config.save();
 
         new Thread(() -> {
             JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
@@ -175,11 +146,28 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
     }
 
     /**
+     * Save the current configuration and shutdown the client
+     */
+    public static void exit() {
+        EventBus.INSTANCE.post(new GameExitEnforcedEvent());
+
+        LOGGER.info("Client shutdown initiated.");
+
+        getInstance().quitGame();
+        World.cleanEnvironment();
+        getConfig().save();
+        GlobalExecutorService.shutdown();
+
+        LOGGER.info("Cleanup done.");
+        startFinalKiller();
+    }
+
+    /**
      * This method is the final one to be called before the client is killed for sure. It gives the rest of the client
      * 10 seconds before it forcefully shuts down everything. This is used to ensure that the client quits when it has
      * to, but in case it does so faster, it won't be killed like that.
      */
-    public static void startFinalKiller() {
+    private static void startFinalKiller() {
         Timer finalKiller = new Timer("Final Death", true);
         finalKiller.schedule(new TimerTask() {
             @Override
@@ -210,19 +198,8 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
      * Prepares and sets up the entire client
      */
     private void init() {
-        try {
-            EventServiceLocator.setEventService(EventServiceLocator.SERVICE_NAME_EVENT_BUS, null);
-            EventServiceLocator.setEventService(EventServiceLocator.SERVICE_NAME_SWING_EVENT_SERVICE, null);
-            EventServiceLocator
-                    .setEventService(EventServiceLocator.SERVICE_NAME_EVENT_BUS, new ThreadSafeEventService());
-            EventServiceLocator.setEventService(EventServiceLocator.SERVICE_NAME_SWING_EVENT_SERVICE,
-                    EventServiceLocator.getEventBusService());
-        } catch (EventServiceExistsException e1) {
-            LOGGER.error("Failed preparing the EventBus. Settings the Service handler happened too late");
-        }
-
         prepareConfig();
-        assert cfg != null;
+        assert config != null;
         try {
             initLogfiles();
         } catch (IOException e) {
@@ -230,8 +207,8 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
             e.printStackTrace(System.err);
         }
 
-        Lang.getInstance().recheckLocale(cfg.getString(Lang.LOCALE_CFG));
-        CrashReporter.getInstance().setConfig(getCfg());
+        Lang.getInstance().recheckLocale(config.getString(Lang.LOCALE_CFG));
+        CrashReporter.getInstance().setConfig(getConfig());
 
         // Report errors of the released version only
         if (DEFAULT_SERVER != Servers.Illarionserver) {
@@ -247,65 +224,27 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
             LOGGER.error("Failed to load sounds and music!");
         }
 
-        game = new Game();
+        boolean isFullscreen = config.getBoolean(CFG_FULLSCREEN);
+        int width = config.getInteger(CFG_WIDTH);
+        int height = config.getInteger(CFG_HEIGHT);
 
-        int width;
-        int height;
-        boolean fullScreen;
-        if (cfg.getBoolean(CFG_FULLSCREEN)) {
-            // Determine the dimensions of the window to create
-            GraphicResolution res = null;
-
-            String resolutionString = cfg.getString(CFG_RESOLUTION);
-            if (resolutionString != null) {
-                try {
-                    res = new GraphicResolution(resolutionString);
-                } catch (@Nonnull IllegalArgumentException ex) {
-                    LOGGER.error("Failed to initialize screen resolution. Falling back.");
-                }
-            }
-
-            if (res == null) {
-                width = 0;
-                height = 0;
-            } else {
-                width = res.getWidth();
-                height = res.getHeight();
-            }
-
-            fullScreen = true;
-        } else {
-            width = cfg.getInteger("windowWidth");
-            height = cfg.getInteger("windowHeight");
-            fullScreen = false;
-        }
-
-        try {
-            // Get the game container used to display the game from the engine, using the dimensions from earlier
-            gameContainer = EngineManager.createDesktopGame(Backend.libGDX, game, width, height, fullScreen);
-        } catch (@Nonnull EngineException e) {
-            LOGGER.error("Fatal error creating game screen!!!", e);
-            System.exit(-1);
-        }
-
+        gameContainer = new ApplicationGameContainer(width, height, isFullscreen);
         gameContainer.setTitle(APPLICATION.getApplicationIdentifier());
         gameContainer.setIcons("illarion_client16.png",
                 "illarion_client32.png",
                 "illarion_client64.png",
                 "illarion_client256.png");
 
-        EventBus.subscribe(CFG_FULLSCREEN, this);
-        EventBus.subscribe(CFG_RESOLUTION, this);
+        EventBus.INSTANCE.register(this);
 
         try {
-            gameContainer.setResizeable(true);
             System.out.println("Startup done.");
             //MacOSx notice: in case AWT cannot be completely removed from the game (error messages etc)
             //it is needed to initialize AWT once (by calling getDefaultToolkit) before initGLFW
-            gameContainer.startGame();
+            gameContainer.startGame(new StateManager(), new Diagnostics(true));
         } catch (@Nonnull Exception e) {
             LOGGER.error("Exception while launching game.", e);
-            exitGameContainer();
+            exit();
         }
     }
 
@@ -313,70 +252,17 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
      * Prepare the configuration system and the decryption system.
      */
     private void prepareConfig() {
-        cfg = new ConfigSystem(getFile("Illarion.xcfgz"));
-        cfg.setDefault("debugLevel", 1);
-        cfg.setDefault("showIDs", false);
-        cfg.setDefault(Player.CFG_SOUND_ON, true);
-        cfg.setDefault(Player.CFG_SOUND_VOL, Player.MAX_CLIENT_VOL);
-        cfg.setDefault("musicOn", true);
-        cfg.setDefault("musicVolume", Player.MAX_CLIENT_VOL * 0.25f);
-        cfg.setDefault(ChatLog.CFG_TEXTLOG, true);
-        cfg.setDefault(CFG_FULLSCREEN, false);
+        config = new ConfigSystem(getFile("Illarion.cfg"),
+        getClass().getResourceAsStream("/default-config.properties"));
 
-        cfg.setDefault("windowWidth", 1280);
-        cfg.setDefault("windowHeight", 1024);
-        cfg.setDefault("savePassword", false);
-        cfg.setDefault("showFps", false);
-        cfg.setDefault("showPing", false);
-        cfg.setDefault(CrashReporter.CFG_KEY, CrashReporter.MODE_ASK);
+        //TODO: Set default for RESOLUTION ASAP
 
-        Locale locale = Locale.getDefault(Category.DISPLAY);
         // If the system locale is german, set to german. Otherwise, default to English
-        if ("de".equals(locale.getLanguage())) {
-            cfg.setDefault(Lang.LOCALE_CFG, Lang.LOCALE_CFG_GERMAN);
-        } else {
-            cfg.setDefault(Lang.LOCALE_CFG, Lang.LOCALE_CFG_ENGLISH);
-        }
-
-        cfg.setDefault("inventoryPosX", "100px");
-        cfg.setDefault("inventoryPosY", "10px");
-        cfg.setDefault("bookDisplayPosX", "150px");
-        cfg.setDefault("bookDisplayPosY", "15px");
-        cfg.setDefault("skillWindowPosX", "200px");
-        cfg.setDefault("skillWindowPosY", "20px");
-        cfg.setDefault("backpackDisplayPosX", "100px");
-        cfg.setDefault("backpackDisplayPosY", "10px");
-        cfg.setDefault("depotDisplayPosX", "150px");
-        cfg.setDefault("depotDisplayPosY", "15px");
-        cfg.setDefault("bagDisplayPosX", "200px");
-        cfg.setDefault("bagDisplayPosY", "20px");
-        cfg.setDefault("questWindowPosX", "100px");
-        cfg.setDefault("questWindowPosY", "100px");
-        cfg.setDefault("questShowFinished", false);
-        cfg.setDefault("server", Servers.Devserver.getServerKey());
-        cfg.setDefault("serverAddress", Servers.Customserver.getServerHost());
-        cfg.setDefault("serverPort", Servers.Customserver.getServerPort());
-        cfg.setDefault("clientVersion", Servers.Customserver.getClientVersion());
-        cfg.setDefault("clientVersionOverwrite", false);
-        cfg.setDefault("serverAccountLogin", true);
-        cfg.setDefault("wasdWalk", true);
-        cfg.setDefault("disableChatAfterSending", true);
-        cfg.setDefault("showQuestsOnGameMap", true);
-        cfg.setDefault("showQuestsOnMiniMap", true);
-        /* Showing the avatar tag on a permanent base.
-         * 0 -> none are shown
-         * 1 -> other players only
-         * 2 -> other players and monsters
-         */
-        cfg.setDefault("showAvatarTagPermanently", 0);
-        cfg.set("limitPathFindingToMouseDirection", true);
-        cfg.set("followMousePathFinding", true);
-        cfg.setDefault("preLoadBagCount", 2);
-
-        cfg.setDefault(Translator.CFG_KEY_PROVIDER, Translator.CFG_VALUE_PROVIDER_NONE);
-        cfg.setDefault(Translator.CFG_KEY_DIRECTION, Translator.CFG_VALUE_DIRECTION_DEFAULT);
-
-        cfg.set("doubleClickInterval", 1000);
+        config.setDefault(Lang.LOCALE_CFG,
+                Locale.getDefault(Category.DISPLAY).getLanguage().equals("de") ?
+                        Lang.LOCALE_CFG_GERMAN :
+                        Lang.LOCALE_CFG_ENGLISH
+                );
 
         Crypto crypt = new Crypto();
         crypt.loadPublicKey();
@@ -429,25 +315,8 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
      */
     @Nonnull
     @Contract(pure = true)
-    public static Config getCfg() {
-        return Objects.requireNonNull(INSTANCE.cfg, "Config is not ready yet");
-    }
-
-    /**
-     * Save the current configuration and shutdown the client
-     */
-    public static void exitGameContainer() {
-        INSTANCE.gameContainer.exitGame();
-
-        LOGGER.info("Client shutdown initiated.");
-
-        getInstance().quitGame();
-        World.cleanEnvironment();
-        getCfg().save();
-        GlobalExecutorService.shutdown();
-
-        LOGGER.info("Cleanup done.");
-        startFinalKiller();
+    public static Config getConfig() {
+        return Objects.requireNonNull(INSTANCE.config, "Config is not ready yet");
     }
 
     /**
@@ -484,12 +353,14 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
         return INSTANCE;
     }
 
+    // TODO: Move to stateManager
     public static void performLogout() {
         LOGGER.info("Logout requested.");
         getInstance().quitGame();
-        INSTANCE.game.enterState(Game.STATE_LOGOUT);
+        //INSTANCE.stateManager.enterState(StateManager.STATE_LOGOUT);
     }
 
+    // TODO: Move to stateManager
     public static void returnToLogin(@Nullable String message) {
         if (message == null) {
             LOGGER.info("Returning to login initiated.");
@@ -497,7 +368,7 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
             LOGGER.info("Returning to login initiated. Reason: {}", message);
         }
 
-        INSTANCE.game.enterState(Game.STATE_LOGIN);
+        //INSTANCE.stateManager.enterState(StateManager.STATE_LOGIN);
     }
 
     /**
@@ -507,7 +378,7 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
      */
     public static void sendDisconnectEvent(@Nonnull String message, boolean tryToReconnect) {
         LOGGER.warn("Disconnect received: {}", message);
-        EventBus.publish(new ConnectionLostEvent(message, tryToReconnect));
+        EventBus.INSTANCE.post(new ConnectionLostEvent(message, tryToReconnect));
     }
 
     /**
@@ -515,7 +386,7 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
      *
      * @return the are used to display the game inside
      */
-    public DesktopGameContainer getContainer() {
+    public GameContainer getContainer() {
         return gameContainer;
     }
 
@@ -539,6 +410,12 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
         usedServer = server;
     }
 
+    @Subscribe
+    public void OnWindowSizeChanged(WindowResizedEvent event) {
+        config.set(CFG_HEIGHT, event.height);
+        config.set(CFG_WIDTH, event.width);
+    }
+
     /**
      * If the config is changed AND the changed config is either the resolution
      * or the fullscreen boolean, updates the relevant config
@@ -550,9 +427,10 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
      * @param topic indicates what in the config to change
      * @param data  the event being handled
      */
-    @Override
     public void onEvent(String topic, ConfigChangedEvent data) {
-        if (CFG_RESOLUTION.equals(topic) || CFG_FULLSCREEN.equals(topic)) {
+        //TODO: Move to game itself, reaction to new UI cfg/resolution change
+        //TODO: Must make Config better accessible for Game
+        /*if (CFG_WIDTH.equals(topic) || CFG_HEIGHT.equals(topic) || CFG_FULLSCREEN.equals(topic)) {
             String resolutionString = cfg.getString(CFG_RESOLUTION);
             if (resolutionString == null) {
                 LOGGER.error("Failed reading new resolution.");
@@ -575,6 +453,6 @@ public final class IllaClient implements EventTopicSubscriber<ConfigChangedEvent
             } catch (@Nonnull EngineException | IllegalArgumentException e) {
                 LOGGER.error("Failed to apply graphic mode: {}", resolutionString);
             }
-        }
+        }*/
     }
 }
