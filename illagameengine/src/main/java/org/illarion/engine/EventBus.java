66dfd3955c07adc2f1cad7a1a614576c0fcfd8b5
r-lodahl <sejyiahn@gmail.com>
2020-12-10 20:13:12 +0100

    Added EventBus based on guava EventBus as enum singleton

---
+++ illagameengine/src/main/java/org/illarion/engine/EventBus.java
@@ -0,0 +1,16 @@
+package org.illarion.engine;
+
+@SuppressWarnings("UnstableApiUsage")
+public enum EventBus {
+    INSTANCE;
+
+    private final com.google.common.eventbus.EventBus eventBus = new com.google.common.eventbus.EventBus();
+
+    public void register(Object object) {
+        eventBus.register(object);
+    }
+
+    public void post(Object event) {
+        eventBus.post(event);
+    }
+}
